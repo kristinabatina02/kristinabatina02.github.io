18b16a64c76713100503bb007560d5c469294809
kristinabatina02 <kiki.batinic02@gmail.com>
2022-12-16 02:27:05 +0100

    web

--- js/main.js
+++ js/main.js
@@ -18,14 +18,6 @@
         $(window).resize(toggleNavbarMethod);
     });
 
-
-    // Date and time picker
-    $('.date').datetimepicker({
-        format: 'L'
-    });
-    $('.time').datetimepicker({
-        format: 'LT'
-    });
     
     
     // Back to top button
@@ -41,31 +33,6 @@
         return false;
     });
 
-
-    // Price carousel
-    $(".price-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 45,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:3
-            }
-        }
-    });
 
 
     // Team carousel
@@ -91,14 +58,6 @@
     });
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        dots: true,
-        loop: true,
-    });
     
 })(jQuery);
 
